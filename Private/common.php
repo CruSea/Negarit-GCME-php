ab4fdc3ba52dca1bd7afff4aac4b9304e6e9078c
Bemni <Bemni>
2017-04-07 08:40:56 -1200

    change database credential

--- Private/common.php
+++ Private/common.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST","localhost:8889");
-define("DB_USER", "bengeos");
-define("DB_PASS", "passben");
-define("DB_NAME", "Negarit");
+define("DB_HOST","localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "negarit");
 ?>
